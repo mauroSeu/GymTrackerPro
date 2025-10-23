4cbb51c674c4ba8645fe1c253d7c26ef483bd33b
mauroSeu <mauro.seu1@gmail.com>
2025-10-24 00:21:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GymTrackerPro/', // 👈 QUESTA RIGA RISOLVE IL PROBLEMA!
   server: {
     port: 3000,
     open: true
